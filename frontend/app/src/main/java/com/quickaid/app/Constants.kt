53e27ac312bca2663172f6b60894d861f7333cb7
han-ha <118191080+han-ha@users.noreply.github.com>
2026-01-27 23:56:02 +0100

    Update Constants.kt

--- frontend/app/src/main/java/com/quickaid/app/Constants.kt
+++ frontend/app/src/main/java/com/quickaid/app/Constants.kt
@@ -1,5 +1,6 @@
 package com.quickaid.app
 
 object Constants {
-    const val BASE_URL_HTTP = "https://jutta-extremer-panlogistically.ngrok-free.dev/api/"
+    const val BASE_URL_HTTP = "" // Adres bazowy HTTP - należy uzupełnić wygenerowanym przez ngroka adresem
+
 }
